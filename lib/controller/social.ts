9519eb4163ca9a773cd1236a1348e20252a9e79c
Gerardo Herrera <geroh@latamcode.com>
2025-02-01 10:26:59 -0300

    Avoid error when the friend list is empty

--- lib/controller/social.ts
+++ lib/controller/social.ts
@@ -1,4 +1,10 @@
-import { Controller, KuzzleRequest, Backend, NameGenerator, BadRequestError } from 'kuzzle';
+import {
+  Controller,
+  KuzzleRequest,
+  Backend,
+  NameGenerator,
+  BadRequestError,
+} from "kuzzle";
 
 export class Social extends Controller {
   constructor(app: Backend) {
@@ -8,45 +14,66 @@ export class Social extends Controller {
     this.definition = {
       actions: {
         bar: {
-          handler: async request => {
-            return `Hello ${request.getString('name')}!`
+          handler: async (request) => {
+            return `Hello ${request.getString("name")}!`;
           },
-          http: [
-            { verb: 'get', path: '/social/bar' },
-          ]
+          http: [{ verb: "get", path: "/social/bar" }],
         },
         getFriends: {
-          handler: async request => {
+          handler: async (request) => {
             try {
               const author_id = request.getKuid();
-              const author_connections = await app.sdk.document.get("social", "connections", author_id);
+              const documentExists = await app.sdk.document.exists(
+                "social",
+                "connections",
+                author_id
+              );
 
-              if (!author_connections._source.friends || author_connections._source.friends.length === 0) {
+              if (!documentExists) {
+                return { friends: [] };
+              }
+
+              const author_connections = await app.sdk.document.get(
+                "social",
+                "connections",
+                author_id
+              );
+
+              if (
+                !author_connections._source.friends ||
+                author_connections._source.friends.length === 0
+              ) {
                 return "You have no friends.";
               }
 
               return {
-                friends: author_connections._source.friends
+                friends: author_connections._source.friends,
               };
             } catch (error) {
               console.log("Error getting friends list", error);
               throw new BadRequestError("Invalid request");
             }
           },
-          http: [
-            { verb: 'get', path: '/social/friends' }
-          ]
+          http: [{ verb: "get", path: "/social/friends" }],
         },
         addFriend: {
-          handler: async request => {
+          handler: async (request) => {
             try {
               const author_id = request.getKuid();
-              const target_id = request.getBodyString('id');
+              const target_id = request.getBodyString("id");
               if (author_id == target_id) {
                 return "Error: Target id is your user id";
               }
-              const target_connections = await app.sdk.document.get("social", "connections", target_id);
-              const author_connections = await app.sdk.document.get("social", "connections", author_id);
+              const target_connections = await app.sdk.document.get(
+                "social",
+                "connections",
+                target_id
+              );
+              const author_connections = await app.sdk.document.get(
+                "social",
+                "connections",
+                author_id
+              );
               if (target_connections._source.blocked.includes(author_id)) {
                 return "Error: The target user has blocked you.";
               }
@@ -61,27 +88,47 @@ export class Social extends Controller {
               }
               target_connections._source.requests.push(author_id);
               author_connections._source.requested.push(target_id);
-              app.sdk.as({ _id: author_id }).document.update("social", "connections", author_id, author_connections._source);
-              app.sdk.as({ _id: target_id }).document.update("social", "connections", target_id, target_connections._source);
+              app.sdk
+                .as({ _id: author_id })
+                .document.update(
+                  "social",
+                  "connections",
+                  author_id,
+                  author_connections._source
+                );
+              app.sdk
+                .as({ _id: target_id })
+                .document.update(
+                  "social",
+                  "connections",
+                  target_id,
+                  target_connections._source
+                );
             } catch (error) {
               console.log(error);
-              throw new BadRequestError("Invalid request")
+              throw new BadRequestError("Invalid request");
             }
           },
-          http: [
-            { verb: 'post', path: '/social/addFriend' },
-          ]
+          http: [{ verb: "post", path: "/social/addFriend" }],
         },
         deleteFriend: {
-          handler: async request => {
+          handler: async (request) => {
             try {
               const author_id = request.getKuid();
-              const target_id = request.getBodyString('id');
+              const target_id = request.getBodyString("id");
               if (author_id == target_id) {
                 return "Error: Target id is your user id";
               }
-              const target_connections = await app.sdk.document.get("social", "connections", target_id);
-              const author_connections = await app.sdk.document.get("social", "connections", author_id);
+              const target_connections = await app.sdk.document.get(
+                "social",
+                "connections",
+                target_id
+              );
+              const author_connections = await app.sdk.document.get(
+                "social",
+                "connections",
+                author_id
+              );
               if (!target_connections._source.friends.includes(author_id)) {
                 return "Error: Target is not your friend";
               }
@@ -89,27 +136,47 @@ export class Social extends Controller {
               target_connections._source.friends.splice(i, 1);
               i = author_connections._source.friends.indexOf(target_id);
               author_connections._source.friends.splice(i, 1);
-              app.sdk.as({ _id: author_id }).document.update("social", "connections", author_id, author_connections._source);
-              app.sdk.as({ _id: target_id }).document.update("social", "connections", target_id, target_connections._source);
+              app.sdk
+                .as({ _id: author_id })
+                .document.update(
+                  "social",
+                  "connections",
+                  author_id,
+                  author_connections._source
+                );
+              app.sdk
+                .as({ _id: target_id })
+                .document.update(
+                  "social",
+                  "connections",
+                  target_id,
+                  target_connections._source
+                );
             } catch (error) {
               console.log(error);
-              throw new BadRequestError("Invalid request")
+              throw new BadRequestError("Invalid request");
             }
           },
-          http: [
-            { verb: 'post', path: '/social/deleteFriend' },
-          ]
+          http: [{ verb: "post", path: "/social/deleteFriend" }],
         },
         acceptFriend: {
-          handler: async request => {
+          handler: async (request) => {
             try {
               const author_id = request.getKuid();
-              const target_id = request.getBodyString('id');
+              const target_id = request.getBodyString("id");
               if (author_id == target_id) {
                 return "Error: Target id is your user id";
               }
-              const target_connections = await app.sdk.document.get("social", "connections", target_id);
-              const author_connections = await app.sdk.document.get("social", "connections", author_id);
+              const target_connections = await app.sdk.document.get(
+                "social",
+                "connections",
+                target_id
+              );
+              const author_connections = await app.sdk.document.get(
+                "social",
+                "connections",
+                author_id
+              );
               if (!target_connections._source.requested.includes(author_id)) {
                 return "Error: Forbidden. You cannot accept a non-existent request.";
               }
@@ -119,27 +186,47 @@ export class Social extends Controller {
               i = author_connections._source.requests.indexOf(target_id);
               author_connections._source.requests.splice(i, 1);
               author_connections._source.friends.push(target_id);
-              app.sdk.as({ _id: author_id }).document.update("social", "connections", author_id, author_connections._source);
-              app.sdk.as({ _id: target_id }).document.update("social", "connections", target_id, target_connections._source);
+              app.sdk
+                .as({ _id: author_id })
+                .document.update(
+                  "social",
+                  "connections",
+                  author_id,
+                  author_connections._source
+                );
+              app.sdk
+                .as({ _id: target_id })
+                .document.update(
+                  "social",
+                  "connections",
+                  target_id,
+                  target_connections._source
+                );
             } catch (error) {
               console.log(error);
-              throw new BadRequestError("Invalid request")
+              throw new BadRequestError("Invalid request");
             }
           },
-          http: [
-            { verb: 'post', path: '/social/acceptFriend' },
-          ]
+          http: [{ verb: "post", path: "/social/acceptFriend" }],
         },
         declineFriend: {
-          handler: async request => {
+          handler: async (request) => {
             try {
               const author_id = request.getKuid();
-              const target_id = request.getBodyString('id');
+              const target_id = request.getBodyString("id");
               if (author_id == target_id) {
                 return "Error: Target id is your user id";
               }
-              const target_connections = await app.sdk.document.get("social", "connections", target_id);
-              const author_connections = await app.sdk.document.get("social", "connections", author_id);
+              const target_connections = await app.sdk.document.get(
+                "social",
+                "connections",
+                target_id
+              );
+              const author_connections = await app.sdk.document.get(
+                "social",
+                "connections",
+                author_id
+              );
               if (!target_connections._source.requested.includes(author_id)) {
                 return "Error: Forbidden. You cannot decline a non-existent request.";
               }
@@ -147,28 +234,48 @@ export class Social extends Controller {
               target_connections._source.requested.splice(i, 1);
               i = author_connections._source.requests.indexOf(target_id);
               author_connections._source.requests.splice(i, 1);
-              app.sdk.as({ _id: author_id }).document.update("social", "connections", author_id, author_connections._source);
-              app.sdk.as({ _id: target_id }).document.update("social", "connections", target_id, target_connections._source);
+              app.sdk
+                .as({ _id: author_id })
+                .document.update(
+                  "social",
+                  "connections",
+                  author_id,
+                  author_connections._source
+                );
+              app.sdk
+                .as({ _id: target_id })
+                .document.update(
+                  "social",
+                  "connections",
+                  target_id,
+                  target_connections._source
+                );
             } catch (error) {
-              throw new BadRequestError("Invalid request")
+              throw new BadRequestError("Invalid request");
             }
           },
-          http: [
-            { verb: 'post', path: '/social/declineFriend' },
-          ]
+          http: [{ verb: "post", path: "/social/declineFriend" }],
         },
         blockUser: {
-          handler: async request => {
+          handler: async (request) => {
             try {
               const author_id = request.getKuid();
-              const target_id = request.getBodyString('id');
+              const target_id = request.getBodyString("id");
               if (author_id == target_id) {
                 return "Error: Target id is your user id";
               }
-              const target_connections = await app.sdk.document.get("social", "connections", target_id);
-              const author_connections = await app.sdk.document.get("social", "connections", author_id);
+              const target_connections = await app.sdk.document.get(
+                "social",
+                "connections",
+                target_id
+              );
+              const author_connections = await app.sdk.document.get(
+                "social",
+                "connections",
+                author_id
+              );
               if (author_connections._source.blocked.includes(target_id)) {
-                return "You have already blocked this user"
+                return "You have already blocked this user";
               }
               let i = target_connections._source.requested.indexOf(author_id);
               target_connections._source.requested.splice(i, 1);
@@ -184,46 +291,71 @@ export class Social extends Controller {
               author_connections._source.friends.splice(i, 1);
 
               author_connections._source.blocked.push(target_id);
-              app.sdk.as({ _id: author_id }).document.update("social", "connections", author_id, author_connections._source);
-              app.sdk.as({ _id: target_id }).document.update("social", "connections", target_id, target_connections._source);
+              app.sdk
+                .as({ _id: author_id })
+                .document.update(
+                  "social",
+                  "connections",
+                  author_id,
+                  author_connections._source
+                );
+              app.sdk
+                .as({ _id: target_id })
+                .document.update(
+                  "social",
+                  "connections",
+                  target_id,
+                  target_connections._source
+                );
             } catch (error) {
-              throw new BadRequestError("Invalid request")
+              throw new BadRequestError("Invalid request");
             }
           },
-          http: [
-            { verb: 'post', path: '/social/blockUser' },
-          ]
+          http: [{ verb: "post", path: "/social/blockUser" }],
         },
         unblockUser: {
-          handler: async request => {
+          handler: async (request) => {
             try {
               const author_id = request.getKuid();
-              const target_id = request.getBodyString('id');
+              const target_id = request.getBodyString("id");
               if (author_id == target_id) {
                 return "Error: Target id is your user id";
               }
-              const author_connections = await app.sdk.document.get("social", "connections", author_id);
+              const author_connections = await app.sdk.document.get(
+                "social",
+                "connections",
+                author_id
+              );
               if (!author_connections._source.blocked.includes(target_id)) {
                 return "User is not bocked.";
               }
               const i = author_connections._source.blocked.indexOf(target_id);
               author_connections._source.blocked.splice(i, 1);
-              app.sdk.as({ _id: author_id }).document.update("social", "connections", author_id, author_connections._source);
+              app.sdk
+                .as({ _id: author_id })
+                .document.update(
+                  "social",
+                  "connections",
+                  author_id,
+                  author_connections._source
+                );
             } catch (error) {
-              throw new BadRequestError("Invalid request")
+              throw new BadRequestError("Invalid request");
             }
           },
-          http: [
-            { verb: 'post', path: '/social/unblockUser' },
-          ]
+          http: [{ verb: "post", path: "/social/unblockUser" }],
         },
         getProfile: {
-          handler: async request => {
+          handler: async (request) => {
             try {
               const author_id = request.getKuid();
               var { target_id } = request.input.args;
               if (!target_id) target_id = author_id;
-              const author_profile = await app.sdk.document.get("social", "profiles", target_id);
+              const author_profile = await app.sdk.document.get(
+                "social",
+                "profiles",
+                target_id
+              );
               if (target_id != author_id) {
                 delete target_id.id;
                 delete target_id.device_token;
@@ -231,33 +363,35 @@ export class Social extends Controller {
               return author_profile;
             } catch (error) {
               console.log("Error getting profile", error);
-              throw new BadRequestError("Invalid request")
+              throw new BadRequestError("Invalid request");
             }
           },
-          http: [
-            { verb: 'get', path: '/social/profile' },
-          ]
+          http: [{ verb: "get", path: "/social/profile" }],
         },
         updateProfile: {
-          handler: async request => {
+          handler: async (request) => {
             try {
               const author_id = request.getKuid();
               const parcial_profile = request.getBody();
               if (parcial_profile == null) {
-                throw new BadRequestError("Missing body")
+                throw new BadRequestError("Missing body");
               }
-              app.sdk.as({ _id: author_id }).document.upsert("social", "profiles", author_id, parcial_profile);
+              app.sdk
+                .as({ _id: author_id })
+                .document.upsert(
+                  "social",
+                  "profiles",
+                  author_id,
+                  parcial_profile
+                );
             } catch (error) {
               console.log("Error updating profile", error);
-              throw new BadRequestError("Invalid request")
+              throw new BadRequestError("Invalid request");
             }
           },
-          http: [
-            { verb: 'post', path: '/social/profile' },
-          ]
+          http: [{ verb: "post", path: "/social/profile" }],
         },
-      }
+      },
     };
   }
 }
-
